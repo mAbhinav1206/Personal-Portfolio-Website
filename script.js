f6558e3fd4e5ff3b389451663067d02f461e4043
Abhinav Mishra <mabhinav1206@gmail.com>
2025-12-23 17:50:52 +0530

    Uploaded local files and assets

---
+++ script.js
@@ -0,0 +1,16 @@
+const cards = document.querySelectorAll(".profile-card");
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.style.opacity = 1;
+            entry.target.style.transform = "translateY(0)";
+        }
+    });
+}, { threshold: 0.2 });
+
+cards.forEach(card => {
+    card.style.opacity = 0;
+    card.style.transform = "translateY(100px)";
+    observer.observe(card);
+});
